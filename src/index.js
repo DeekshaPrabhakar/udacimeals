f06fea020891811ccae1534b6b79abcd29868125
Deeksha Prabhakar <deekiyengar@gmail.com>
2017-10-30 13:21:39 -0700

    feat: initial commit
    vanilla redux code for Udacimeals app before adding react-redux
    framework and using providers

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './components/App';
+import registerServiceWorker from './registerServiceWorker';
+import { createStore } from 'redux';
+import reducer from './reducers'
+
+const store = createStore(reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)  
+
+ReactDOM.render(<App store={store} />, document.getElementById('root'));
+registerServiceWorker();
